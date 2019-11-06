da1cbf858836e66d26c9d4d5ef1b037bf2c69397
Abisheik <abisheik414@gmail.com>
2019-11-06 16:57:36 -0500

    Initial commit

---
+++ 100653409 - (Individual Assignment 2) Mail Pilot/Assets/_Scripts/BulletController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed = 0.1f;
+    public Boundary boundary;
+
+    //TODO: create a reference to the BulletPoolManager
+    private BulletPoolManager BulletPoolMan;
+
+    void Start()
+    {
+        boundary.Top = 2.45f;
+        BulletPoolMan = GameObject.FindObjectOfType<BulletPoolManager>(); // Looks for an active object that is of the Bullet Pool Manager class
+                                                                          // This is done to help the Reset Bullet function to look for the correct object and set it to not active
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0.0f, bulletSpeed, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y >= boundary.Top)
+        {
+            //TODO: This code needs to change to use the BulletPoolManager's
+            //TODO: ResetBullet function which will return the bullet to the pool
+            BulletPoolMan.ResetBullet(this.gameObject); // Calls the reset function from the Bullet Pool Manager class
+        }
+    }
+}
---
+++ 100653409 - (Individual Assignment 2) Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+
+    //TODO: create a structure to contain a collection of bullets
+    public int bulletAmount = 50;
+    public Queue<GameObject> bulletList = new Queue<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: add a series of bullets to the Bullet Pools
+        for (int i = 0; i < bulletAmount; i++)
+        {
+            GameObject bulletToShoot = Instantiate(bullet);
+            bulletToShoot.SetActive(false);
+            bulletList.Enqueue(bulletToShoot);
+        }
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet(Vector3 pos, Quaternion id)
+    {
+        GameObject bulletToGet = bulletList.Dequeue(); // Create new game object and make it equal to the bullet that is getting dequeued
+        bulletToGet.SetActive(true); // Set the new game object to active
+        bulletToGet.transform.position = pos;
+        bulletToGet.transform.rotation = id;
+        return bulletToGet;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet) 
+    {
+        bulletList.Enqueue(bullet); // Add the game object to the queue to make the bullet pool equal to 50
+        bullet.SetActive(false); //Sets the current active bullet to not active
+    }
+}
